86307e7393c9647106d22f26a76720ed871b8afc
Bruno Koster <bruno10762@gmail.com>
2021-08-17 17:59:30 -0300

    Update navbar

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  const btnMobile = $('#btn-mobile')
+  const nav = $('#nav')
+  const menu = $('#menu')
+  const logo = $('#logo')
+
+  function toggleMenu() {
+    $(nav).toggleClass('active')
+
+    const active = $(nav).hasClass('active')
+    let msg 
+
+    $(btnMobile).attr('aria-expanded', active);
+
+    if (active) {
+      msg = 'Fechar menu'
+    } else {
+      msg = 'Abrir menu'
+    }
+
+    $(btnMobile).attr('aria-label', msg)
+  }
+  
+  $(btnMobile).click(toggleMenu)
+  $(menu).click(toggleMenu)
+  $(logo).click(function () { 
+    const active = $(nav).hasClass('active')
+
+    if (active) {
+      toggleMenu()
+    }
+  });
+})
